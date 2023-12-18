7d96483877f51b493a75180f0d49feb66d48af54
pr3Cash <104692055+pr3Cash@users.noreply.github.com>
2023-12-18 22:19:16 +0530

    event emmiters

---
+++ nodeJS/modules/event_emmiters.js
@@ -0,0 +1,47 @@
+//EventEmitter
+// const EventEmitter = require('events');
+
+// const customEmitter = new EventEmitter();
+
+// customEmitter.on("res",(name,id)=>{
+//     console.log(`hello ${name} ${id}`)
+// });
+// customEmitter.on("res",()=>{
+//     console.log("hey")
+// });
+
+// customEmitter.emit("res","bhanu",68)
+
+
+//http example
+
+const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     res.end("hello");
+// }).listen(5000)
+
+//using event emitter api 
+const server = http.createServer()
+//emits request event 
+server.on('request',(req,res)=>{
+    res.end('Hello bhanu')  
+})
+
+server.listen(5000)
+
+
+
+// const server = http.createServer((req,res)=>{
+
+//     if (req.url === '/abc') {
+//         res.end(req.url)    
+//     }
+//     if (req.url === '/') {
+//         res.end('./start.html')    
+//         res.pipe;
+//     }
+
+//     res.end(`<h1> oops that messed up! got into wrong file`)
+// }).listen(5000)
+
